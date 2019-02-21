7ad0bc7002017c63ffc0938f60e3dd6b3f8865a9
Sara Anderson <anderson.2934@osu.edu>
2019-02-20 20:55:25 -0500

    update a book - lab 5

--- api/Startup.cs
+++ api/Startup.cs
@@ -43,7 +43,6 @@ namespace Fisher.Bookstore.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
             app.UseHttpsRedirection();
             app.UseMvc();
         }
