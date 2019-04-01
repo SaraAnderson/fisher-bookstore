50da5034b05958a76ccba52d64fed2d3f9ee0c23
Sara Anderson <anderson.2934@osu.edu>
2019-04-01 01:40:00 -0400

    Tests for Book

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -13,5 +13,22 @@ namespace Fisher.Bookstore.Models
         public string ISBN { get; set; }
         
         public DateTime publicationDate { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public void ChangePublicationDate(DateTime dateTime)
+        {
+            this.PublishDate = dateTime;
+        }
+
+        public void ChangePublisher(string newPublisher)
+        {
+            this.Publisher = newPublisher;
+        }
+
+        public void ChangeTitle(string newTitle)
+        {
+            this.Title = newTitle;
+        }
     }
 }
